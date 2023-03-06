b6a8317b6473f5991a9203d510c6f09b85f48258
unknown <krystiangruszecki50@gmail.com>
2023-03-06 18:55:35 +0100

    added basic menu linking to corresponding views

--- scrapper/urls.py
+++ scrapper/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 app_name = 'scrapper'
 urlpatterns = [
+    path('', views.home_page, name='home_page'),
+    path('ekstrakcja/', views.ekstrakcja, name='ekstrakcja'),
+    path('lista/', views.lista, name='lista'),
+    path('autor/', views.autor, name='autor')
 ]
--- scrapper/views.py
+++ scrapper/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(request):
+    return render(request, 'scrapper/index.html')
+
+def ekstrakcja(request):
+    return render(request, 'scrapper/ekstrakcja.html')
+
+def lista(request):
+    return render(request, 'scrapper/index.html')
+
+def autor(request):
+    return render(request, 'scrapper/index.html')
